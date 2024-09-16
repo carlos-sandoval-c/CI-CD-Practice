372646c516ffb3e4ce4f5cbb7d5a5db55e88c5a9
Carlos Sandoval <c.sandoval@globant.com>
2024-09-16 15:58:32 -0500

    feat(actions): add headless option in driver

--- src/test/java/com/globant/utils/DriverManager.java
+++ src/test/java/com/globant/utils/DriverManager.java
@@ -5,7 +5,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverManager {
     private final WebDriver driver;
@@ -21,12 +23,20 @@ public class DriverManager {
         switch (browser.toLowerCase()) {
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
-                this.driver = new FirefoxDriver();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--no-sandbox");
+                firefoxOptions.addArguments("--disable-dev-shm-usage");
+                firefoxOptions.addArguments("--headless");
+                this.driver = new FirefoxDriver(firefoxOptions);
                 break;
             case "chrome":
             default:
                 WebDriverManager.chromedriver().setup();
-                this.driver = new ChromeDriver();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--no-sandbox");
+                chromeOptions.addArguments("--disable-dev-shm-usage");
+                chromeOptions.addArguments("--headless");
+                this.driver = new ChromeDriver(chromeOptions);
                 break;
         }
     }
